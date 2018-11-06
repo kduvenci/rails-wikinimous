d4b0835bf60307c1ec35bb8993fe389a87810d7e
kemal duvenci <k.duvenci@gmail.com>
2018-11-07 02:12:24 +0900

    index completed

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,36 @@
+class ArticlesController < ApplicationController
+  before_action :set_article, only: [:edit, :update, :show, :destroy]
+  # resources :articles
+  def index
+    @articles = Article.all
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def strong_params
+    # soon
+  end
+
+end
